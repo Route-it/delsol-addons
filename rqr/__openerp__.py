b3844fa557669d794d3afac35712f885c23846ca
unknown <sebaseifert@gmail.com>
2015-12-29 15:00:45 -0300

    initial commit

---
+++ rqr/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "rqr",
+
+    'summary': """
+        Modulo de trackeo RQRS Ford""",
+
+    'description': """
+        Con este modulo se aborda el trackeo de la recepción de RQR´s de parte de los clientes de la concesionaria Ford desde el momento de 
+        la emisión del remito de entrega del vehículo hasta la encuesta realizada por Ford. 
+    """,
+
+    'author': "RouteIT",
+    'website': "http://www.routeit.com.ar",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views.xml',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
